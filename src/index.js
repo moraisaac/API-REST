7afd4412f72c75c22488ae915cf66b79e0ae241e
Isaac Mora <izaaq.mora@gmail.com>
2022-08-25 16:44:39 -0500

    Correccion desafio

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express()
 const router = Router()
 const PORT = 8080
 
-app.use(express.json()) 
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -20,35 +20,37 @@ router.get('/', (req, res) => {  // GET '/api/productos' -> devuelve todos los p
 router.get('/:id', (req, res) => {  // GET '/api/productos/:id' -> devuelve un producto según su id.
     const { id } = req.params
     const prod = productos.filter(prod => prod.id == id)
-    if ( prod.length == 0 ) res.status(400).json({ error : 'producto no encontrado' })
+    if (prod.length == 0) res.status(400).json({ error: 'producto no encontrado' })
     res.send(prod)
 })
 
 router.post('/', (req, res) => {  // POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
     const { nombre, precio, urlImage } = req.body
-    if ( !nombre || !precio || !urlImage ) res.status(400).json({ error : 'producto no encontrado' })
-    const data = { nombre, precio, urlImage }
-    data.id = prodID
-    productos.push(data)
-    prodID += 1
-    res.send(data)
+    if (!nombre || !precio || !urlImage) {
+        const data = { nombre, precio, urlImage }
+        data.id = prodID
+        productos.push(data)
+        prodID += 1
+        res.send(data)
+    } else { res.status(400).json({ error: 'producto no encontrado' }) }
 })
 
 router.put('/:id', (req, res) => {  // PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
     const { id } = req.params
     const { nombre, precio, urlImage } = req.body
-    if ( !nombre || !precio || !urlImage ) res.status(400).json({ error : 'producto no encontrado' })
-    let contador = 0
-    for ( let i = 0; i < productos.length; i++) {
-        if (productos[i].id == id) {
-            productos[i].nombre = nombre
-            productos[i].precio = precio
-            productos[i].urlImage = urlImage
-            contador += 1
-            break
+    if (!nombre || !precio || !urlImage) {
+        let contador = 0
+        for (let i = 0; i < productos.length; i++) {
+            if (productos[i].id == id) {
+                productos[i].nombre = nombre
+                productos[i].precio = precio
+                productos[i].urlImage = urlImage
+                contador += 1
+                break
+            }
         }
-    }
-    if ( contador == 0 ) res.status(400).json({ error : 'producto no encontrado' })
+    } else { res.status(400).json({ error: 'producto no encontrado' }) }
+    if (contador == 0) res.status(400).json({ error: 'producto no encontrado' })
     res.send(productos)
 })
 
@@ -60,7 +62,7 @@ router.delete('/:id', (req, res) => {  // DELETE '/api/productos/:id' -> elimina
             productos.splice(i, 1)
         }
     }
-    if ( productos.length == largoActual ) res.status(400).json({ error : 'producto no encontrado' })
+    if (productos.length == largoActual) res.status(400).json({ error: 'producto no encontrado' })
     res.send(productos)
 })
 
